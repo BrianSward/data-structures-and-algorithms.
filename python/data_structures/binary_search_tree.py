23925f1de09da88bb2f839b4a4c401a058e562d2
Brian Sward <bsward@gmail.com>
2023-01-08 09:22:24 -0800

    class 15 - bt and bst

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,15 +1,61 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
-    """
-    Put docstring here
-    """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
-        # method body here
-        pass
+    # had trouble getting recursion working, had to do it with iterations
+
+    # def add(self, value):
+    #     test = Node(value)
+    #     if self.root is None:
+    #         self.root = test
+    #         return
+    #     if self.root.value < test.value:
+    #         if test.left is None:
+    #             test.left = Node(value)
+    #         else:
+    #             self.add(value, test.left)
+    #
+    #     elif self.root.value > test.value:
+    #         if test.right is None:
+    #             test.right = Node(value)
+    #         else:
+    #             self.add(value, test.right)
+
+    # had help with this from a ta as we talked through code
+
+    def add(self, value):
+        node = Node(value)
+        if self.root is None:
+            self.root = node
+            return
+        prev = None
+        temp = self.root
+        while temp is not None:
+            if temp.value > value:
+                prev = temp
+                temp = temp.left
+            elif temp.value < value:
+                prev = temp
+                temp = temp.right
+        if prev.value > value:
+            prev.left = node
+        else:
+            prev.right = node
+
+
+    def contains(self, value):
+        current_node = self.root
+        while current_node is not None:
+            if value == current_node.value:
+                return True
+            elif value < current_node.value:
+                current_node = current_node.left
+            else:
+                current_node = current_node.right
+        return False
+
+
--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,16 +1,106 @@
-class BinaryTree:
-    """
-    Put docstring here
-    """
-
-    def __init__(self):
-        # initialization here
-        pass
-
-    def some_method(self):
-        # method body here
-        pass
-
-
 class Node:
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree():
+    def __init__(self):
+        self.root = None
+
+    def post_order(self, root=None, nodes=None):
+        """
+        return a list of nodes in a binary tree in post order
+        given below, return [4, 5, 2, 6, 7, 3, 1]
+        """
+
+        # method
+        # initially start at root
+        if root is None:
+            root = self.root
+
+        if nodes is None:
+            nodes = []
+
+        # left child
+        if root.left:
+            self.post_order(root.left, nodes)
+        # right child
+        if root.right:
+            self.post_order(root.right, nodes)
+        # root
+        nodes.append(root.value)
+
+        return nodes
+
+    def pre_order(self, root=None, nodes=None):
+        """
+        return a list of nodes in a binary tree in pre order
+        given below, return [1, 2, 4, 5, 3, 6, 7]
+        """
+        # method
+        # initially start at root
+        if root is None:
+            root = self.root
+        # root
+        if nodes is None:
+            nodes = []
+
+        nodes.append(root.value)
+
+        # left child
+        if root.left:
+            self.pre_order(root.left, nodes)
+        # right child
+        if root.right:
+            self.pre_order(root.right, nodes)
+
+        return nodes
+
+    def in_order(self, root=None, nodes=None):
+        """
+        return a list of nodes in a binary tree in order
+        given below, return [4, 2, 5, 1, 6, 3, 7]
+        """
+        # method
+        # initially start at root
+        if root is None:
+            root = self.root
+        if nodes is None:
+            nodes = []
+        # left child
+        if root.left:
+            self.in_order(root.left, nodes)
+
+        # root
+        nodes.append(root.value)
+
+        # right child
+        if root.right:
+            self.in_order(root.right, nodes)
+
+        return nodes
+
+
+if __name__ == "__main__":
+    """
+            1
+          /   \
+         2     3
+        / \   / \
+       4   5 6   7
+    """
+    bt_1 = BinaryTree()
+    bt_1.root = Node(1)
+    bt_1.root.left = Node(2)
+    bt_1.root.right = Node(3)
+    bt_1.root.left.left = Node(4)
+    bt_1.root.left.right = Node(5)
+    bt_1.root.right.left = Node(6)
+    bt_1.root.right.right = Node(7)
+    print("post order: ", bt_1.post_order())
+    print("")
+    print("pre order: ", bt_1.pre_order())
+    print("")
+    print("in order: ", bt_1.in_order())
